ce7267262a3c781306f4847856cf3108666c2dac
Liav <liavt242@gmail.com>
2023-01-30 16:55:15 +0200

    possible exceptions

--- src/gcc_exceptions.py
+++ src/gcc_exceptions.py
@@ -10,3 +10,18 @@ class UserError(GccErrors):
 class InvalidEmail(GccErrors):
     def __init__(self):
         super().__init__('Invalid Email.')
+
+
+class InvalidRole(GccErrors):
+    def __init__(self):
+        super().__init__('Invalid Role.')
+
+
+class CourseStateError(GccErrors):
+    def __init__(self):
+        super().__init__('course_state must be one of [PROVISIONED, ACTIVE, ARCHIVED, DECLINED, SUSPENDED].')
+
+
+class AlreadyInCache(GccErrors):
+    def __init__(self):
+        super().__init__('course already made.')
