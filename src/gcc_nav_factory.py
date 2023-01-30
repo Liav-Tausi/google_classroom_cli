b757055853ce8a861f78f5a31570852fc9cddcc3
Liav <liavt242@gmail.com>
2023-01-30 16:55:37 +0200

    future factory navigator

---
+++ src/gcc_nav_factory.py
@@ -0,0 +1,13 @@
+from gcc_base import GccBase
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+import gcc_validators
+import gcc_exceptions
+
+
+class NavFactory(GccBase):
+
+    @staticmethod
+    def create_course(self):
+        pass
