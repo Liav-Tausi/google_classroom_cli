9ca236080f21268b7d5f8802b3c33e57e44eae57
Liav <liavt242@gmail.com>
2023-01-30 16:56:01 +0200

    teacher user

--- src/gcc_teacher.py
+++ src/gcc_teacher.py
@@ -10,8 +10,10 @@ import gcc_validators
 
 class Student(GccBase):
 
-    def __init__(self, email: str, role='teacher'):
-        super().__init__(role)
-        if not gcc_validators.is_email:
+    def __init__(self, email: str, path_to_creds: str, role: str = 'teacher'):
+        if role != 'teacher':
+            raise gcc_exceptions.InvalidRole()
+        super().__init__(role, path_to_creds)
+        if not gcc_validators.is_email(email):
             raise gcc_exceptions.InvalidEmail()
         self.__email: str = email
