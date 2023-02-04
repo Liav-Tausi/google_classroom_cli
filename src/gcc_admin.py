17ebb0d579c0e56e132757433b23de23335cac5a
Liav <liavt242@gmail.com>
2023-02-04 22:13:33 +0200

    admin user #added del, list, get teacher

--- src/gcc_admin.py
+++ src/gcc_admin.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from gcc_base import GccBase
@@ -12,13 +13,12 @@ from googleapiclient.errors import HttpError
 
 class Admin(GccBase):
 
-    def __init__(self, email: str, role: str = 'admin'):
+    def __init__(self, role: str = 'admin', email: str = None,
+                 work_space: str = None, ref_cache_month: int = 12):
+
         if role != 'admin':
             raise gcc_exceptions.InvalidRole()
-        super().__init__(email, role)
-        if not gcc_validators.is_email(email):
-            raise gcc_exceptions.InvalidEmail()
-        self.__email: str = email
+        super().__init__(role, ref_cache_month, work_space, email)
 
     @GccBase.cacher
     def detailed_create_course(self) -> tuple:
@@ -29,18 +29,18 @@ class Admin(GccBase):
         with open('data/detailed_create_course.json', 'r') as fh:
             course_json = json.load(fh)
         try:
-            course = self.classroom.courses().create(body=course_json).execute()
+            request = self.classroom.courses().create(body=course_json).execute()
             print(f"""
-            Created course: {course["name"]}
-            Course id: {course["id"]}
+            Created course: {request["name"]}
+            Course id: {request["id"]}
             """)
-            return course['id'], course["name"]
+            return request['id'], request["name"]
         except HttpError as error:
             print('An error occurred: %s' % error)
 
     @GccBase.cacher
     def create_course(self, name: str, section: str, description: str, room: str, owner_id='me',
-                      course_state: str = 'PROVISIONED'):
+                      course_state: str = 'PROVISIONED') -> tuple:
         """
         Creates a course
 
@@ -68,35 +68,91 @@ class Admin(GccBase):
             'courseState': course_state.upper()
         }
         try:
-            course = self.classroom.courses().create(body=course).execute()
+            request = self.classroom.courses().create(body=course).execute()
             print(f"""
-                   Created course: {course["name"]}
-                   Course id: {course["id"]}
+                   Created course: {request["name"]}
+                   Course id: {request["id"]}
                    """)
-            return course['id'], course["name"]
+            return  request['id'],  request["name"], self.email
         except HttpError as error:
             print('An error occurred: %s' % error)
 
-    def add_teacher(self, course_id: int, teacher_email: str) -> True:
-        # if course_id not in self.cache:
-        #     raise gcc_exceptions.NotInCache()
-        if not gcc_validators.is_email(teacher_email):
+    def add_teacher(self, course_id: int, teacher_email: str) -> None:
+        if self.workspace:
+            check = self.workspace
+        else:
+            check = self.email
+        if str(course_id) not in self.cache[check]:
+            raise gcc_exceptions.NotInCache()
+        if not gcc_validators.is_email(teacher_email) and teacher_email != 'me':
             raise gcc_exceptions.InvalidEmail()
         data: dict = {
-                "userId": teacher_email
-                }
+            "userId": teacher_email,
+        }
         try:
-            teachers = self.classroom.courses().teachers()
-            teacher = teachers.create(courseId=course_id, body=data).execute()
+            request = self.classroom.courses().teachers().create(courseId=course_id,
+                                                                 body=data).execute()
+
             print('User %s was added as a teacher to the course with ID %s'
-                  % (teacher.get('profile').get('name').get('fullName'),
+                  % (request.get('profile').get('name').get('fullName'),
                      course_id))
-            return True
+
+            self._update_cache((course_id, request.get('profile').get('name').get('fullName'), teacher_email),
+                               datetime.datetime.now().timestamp())
         except HttpError as error:
             print('An error occurred: %s' % error)
+
+    def delete_teacher(self, course_id: int, teacher_email: str) -> None:
+        if teacher_email not in self.cache[self.email][str(course_id)]['teachers']:
+            raise gcc_exceptions.TeacherNotInCourse()
+        if not gcc_validators.is_email(teacher_email):
+            raise gcc_exceptions.InvalidEmail()
+        try:
+            self.classroom.courses().teachers().delete(courseId=str(course_id),
+                                                       userId=teacher_email).execute()
+            self._update_cache(result=(str(course_id),
+                                       None, teacher_email), del_teacher=True)
+
+            print(f'User {teacher_email} was deleted from course with ID {course_id}')
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
+
+    def get_teacher(self, course_id: int, teacher_email: str) -> None:
+        if str(course_id) not in self.cache[self.email]:
+            raise Exception()
+        if not gcc_validators.is_email(teacher_email):
+            raise gcc_exceptions.InvalidEmail()
+        try:
+            request = self.classroom.courses().teachers().get(courseId=course_id,
+                                                              userId=teacher_email).execute()
+            return request
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
+
+    def list_teachers(self, course_id: int, page_size: int = 10, page_token: str = None):
+        if course_id not in self.cache[self.email]:
+            raise Exception()
+        if page_size >= 30:
+            raise gcc_exceptions.ExceededLimitError()
+        try:
+            request = self.classroom.courses().teachers().list(courseId=course_id,
+                                                               pageSize=page_size).execute()
+            if page_token is not None:
+                request.pageToken = page_token
+            teacher = request.execute()
+            return teacher
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
+
 
 
 if __name__ == '__main__':
     gcc = Admin(email='liavt242@gmail.com')
     # liav = gcc.create_course(name='daniel', section='liav', description='liav', room='fee')
-    gcc.add_teacher(course_id=542083844142, teacher_email=r'liavtest36@gmail.com')
+    # gcc.add_teacher(course_id=liav[0])
+    # gcc.delete_teacher(course_id=542178731078, teacher_email='liavt242@gmail.com')
+    print(gcc.get_teacher(course_id=588714514132, teacher_email='liavt242@gmail.com'))
+
