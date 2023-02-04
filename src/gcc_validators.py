d0c581baeca41022547384d59914f1cb81e071df
Liav <liavt242@gmail.com>
2023-02-04 22:14:51 +0200

    necessary validators

--- src/gcc_validators.py
+++ src/gcc_validators.py
@@ -1,11 +1,22 @@
 import re
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-
 
 # email validator
 def is_email(email: str) -> bool:
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
     if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+
+# work_space account validator
+def is_work_space_email(email: str) -> bool:
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    match = re.search(pattern, email)
+    if match:
         return True
     else:
         return False
