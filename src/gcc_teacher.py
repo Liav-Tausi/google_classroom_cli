792fefcf4c65045b64c23a6f7fc35ef163900c62
Liav <liavt242@gmail.com>
2023-02-17 17:40:01 +0200

    teacher user #added course work functions

--- src/gcc_teacher.py
+++ src/gcc_teacher.py
@@ -1,3 +1,5 @@
+import json
+
 from gcc_base import GccBase
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -35,7 +37,7 @@ class Teacher(GccBase):
         :param state: announcement state 
                https://developers.google.com/classroom/reference/rest/v1/courses.announcements#AnnouncementState
                     
-        :param course_id: string
+        :param course_id: either identifier of the course or assigned alias. 'string'
         :param announcement_text: sting
 
         :param materials: materials object
@@ -81,8 +83,8 @@ class Teacher(GccBase):
 
         query-parameters = https://developers.google.com/classroom/reference/rest/v1/courses.announcements/delete#query-parameters
 
-        :param course_id: Course's id string
-        :param announcement_id: Announcement's id string
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :param announcement_id: Announcement's id 'string'
         :return: True
         """
         gcc_validators.are_params_string(course_id, announcement_id)
@@ -106,14 +108,14 @@ class Teacher(GccBase):
 
         query-parameters = https://developers.google.com/classroom/reference/rest/v1/courses.announcements/get#query-parameters
 
-        :param course_id: course's id string
-        :param announcement_id: announcement's id string
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :param announcement_id: announcement's id 'string'
         :return: request dict | False
         """
         gcc_validators.are_params_string(course_id, announcement_id)
 
         try:
-            request = self.classroom.courses().announcements().get(
+            request: dict = self.classroom.courses().announcements().get(
                 courseId=course_id,
                 id=announcement_id
             )
@@ -154,7 +156,7 @@ class Teacher(GccBase):
         if page_token:
             query_params['pageToken'] = page_token
         try:
-            request = self.classroom.courses().announcements().list(**query_params).execute()
+            request: dict = self.classroom.courses().announcements().list(**query_params).execute()
             announcements = request.get("courses", [])
             next_page_token = request.get("nextPageToken", None)
             self._update_cache()
@@ -170,7 +172,7 @@ class Teacher(GccBase):
         see https://developers.google.com/classroom/reference/rest/v1/courses.announcements/modifyAssignees
         for more info
 
-        :param course_id: Identifier of the course. 'string'
+        :param course_id: either identifier of the course or assigned alias. 'string'
         :param announcement_id: Identifier of the announcement. 'string'
         :param assignee_mode: modes of assigning coursework/announcements. ['ASSIGNEE_MODE_UNSPECIFIED',
                                                                             'ALL_STUDENTS',
@@ -193,17 +195,17 @@ class Teacher(GccBase):
         }
 
         try:
-            request = self.classroom.courses().announcements().modify_assignment(
+            request: dict = self.classroom.courses().announcements().modify_assignment(
                 courseId=course_id,
                 id=announcement_id,
                 body=announcement
             ).execute()
 
             return request
-
         except HttpError as error:
             self.logger.error('An error occurred: %s' % error)
             return False
+
 
     def patch_announcement(self, course_id: str, announcement_id: str, new_state: str = None,
                            new_text: str = None, new_scheduled_time: str = None) -> dict | False:
@@ -212,7 +214,7 @@ class Teacher(GccBase):
         see https://developers.google.com/classroom/reference/rest/v1/courses.announcements/patch
         for more info
 
-        :param course_id: Identifier of the course. 'string'
+        :param course_id: either identifier of the course or assigned alias. 'string'
         :param announcement_id: Identifier of the announcement. 'string'
         :param new_state: new announcement state ['ANNOUNCEMENT_STATE_UNSPECIFIED', 'PUBLISHED', 'DRAFT', 'DELETED']
         :param new_text: new announcement text
@@ -243,7 +245,7 @@ class Teacher(GccBase):
         update_mask = update_mask.rstrip(',')
 
         try:
-            request = self.classroom.courses().announcements().patch(
+            request: dict = self.classroom.courses().announcements().patch(
                 courseId=course_id,
                 id=announcement_id,
                 updateMask=update_mask,
@@ -253,6 +255,164 @@ class Teacher(GccBase):
         except HttpError as error:
             self.logger.error('An error occurred: %s' % error)
             return False
+
+
+    def detailed_course_work_create(self, course_id: str, course_json: bool = False) -> dict | False:
+        """
+        this func defines the detailed_course_work_create method, creates course work..
+        see https://developers.google.com/classroom/reference/rest/v1/courses.courseWork/create
+        for more info
+
+        for detailed_course_work_create method the "detailed_course_work.json" stored at the data directory should be filled!!
+        for more info: https://developers.google.com/classroom/reference/rest/v1/courses.courseWork#CourseWork
+
+        :param course_json: flag for indication if json is full True if not False 'bool'
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :return: request dict or False
+
+        """
+        if course_json:
+            raise gcc_exceptions.CourseJsonEmpty()
+
+        with open('data/detailed_course_work.json', 'r') as fh:
+            body = json.load(fh)
+        try:
+            request: dict = self.classroom.courses().courseWork().create(courseId=course_id,
+                                                                         body=body).execute()
+            return request
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+
+    def quick_course_work_create(self, course_id: str, title: str, description: str,
+                                 material: list, work_type: str, state: str) -> dict | False:
+        """
+        this func defines the quick_course_work_create method, creates course work..
+        see https://developers.google.com/classroom/reference/rest/v1/courses.courseWork/create
+        for more info
+
+        :param material: a list with a martial object see: https://developers.google.com/classroom/reference/rest/v1/Material
+        :param state: the state of the course_work see: https://developers.google.com/classroom/reference/rest/v1/courses.courseWork#CourseWorkState
+        :param work_type: type of the course_work see: https://developers.google.com/classroom/reference/rest/v1/CourseWorkType
+        :param description: a course_work description 'string'
+        :param title: a course_work title 'string'
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :return: request dict or False
+
+        """
+        gcc_validators.are_params_string(course_id, title, description, work_type, state)
+
+        if state not in ['COURSE_WORK_STATE_UNSPECIFIED', 'PUBLISHED', 'DRAFT', 'DELETED']:
+            raise gcc_exceptions.CourseWorkStateError()
+
+        if work_type not in ['COURSE_WORK_TYPE_UNSPECIFIED', 'ASSIGNMENT',
+                             'SHORT_ANSWER_QUESTION', 'MULTIPLE_CHOICE_QUESTION']:
+            raise gcc_exceptions.CourseWorkTypeError()
+
+        if isinstance(material, list):
+            raise TypeError()
+
+        body: dict = {
+            'title': title,
+            'description': description,
+            'materials': material,
+            'workType': work_type,
+            'state': state,
+        }
+        try:
+            request: dict = self.classroom.courses().courseWork().create(courseId=course_id,
+                                                                         body=body).execute()
+            return request
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+
+    def delete_course_work(self, course_id: str, course_work_id: str) -> bool:
+        """
+        this func defines the delete_course_work method, deletes course work..
+        see https://developers.google.com/classroom/reference/rest/v1/courses.courseWork/delete
+        for more info
+
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :param course_work_id: identifier of the course work. 'string'
+        :return: True or False 'bool'
+        """
+        gcc_validators.are_params_string(course_id, course_work_id)
+        try:
+            self.classroom.courses().courseWork().delete(courseId=course_id,
+                                                         id=course_work_id).execute()
+            return True
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+    def get_course_work(self, course_id: str, course_work_id: str) -> bool:
+        """
+        this func defines the get_course_work method, gets course work..
+        see https://developers.google.com/classroom/reference/rest/v1/courses.courseWork/get
+        for more info
+
+        :param course_id: either identifier of the course or assigned alias. 'string'
+        :param course_work_id: identifier of the course work. 'string'
+        :return: True or False 'bool'
+        """
+        gcc_validators.are_params_string(course_id, course_work_id)
+        try:
+            self.classroom.courses().courseWork().get(courseId=course_id,
+                                                      id=course_work_id).execute()
+            return True
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+    def list_course_work(self, course_id: str, course_work_states: list[str] = None,
+                         order_by: str = 'updateTime desc', page_size: int = 10,
+                         page_token: str = None) -> tuple[list[dict], str] | False:
+        """
+        this func defines the get_course_work method, Returns a list of course work that the requester is permitted to view.
+        see https://developers.google.com/classroom/reference/rest/v1/courses.courseWork/list
+        for more info
+
+        :param course_id: either identifier of the course or assigned alias. 'string'
+
+        :param course_work_states: Restriction on the work status to return. Only courseWork that matches is returned.
+                                   If unspecified, items with a work status of PUBLISHED is returned.
+
+        :param page_size: Maximum number of items to return. Zero or unspecified indicates that the server may assign a maximum.
+        :param order_by: Optional sort ordering for results
+        :return: Tuple with a list of course work and nextPageToken value
+        """
+        gcc_validators.are_params_string(course_id)
+        if course_work_states is not None:
+            for state in course_work_states:
+                if state not in ['COURSE_WORK_STATE_UNSPECIFIED', 'PUBLISHED', 'DRAFT', 'DELETED']:
+                    raise gcc_exceptions.CourseWorkStateError()
+
+        gcc_validators.are_params_int(page_size)
+
+        query_params = {'courseId': course_id}
+        if course_work_states:
+            query_params['courseWorkStates'] = course_work_states
+        if order_by:
+            query_params['orderBy'] = order_by
+        if page_size:
+            query_params['pageSize'] = page_size
+        if page_token:
+            query_params['pageToken'] = page_token
+
+        try:
+            request: dict = self.classroom.courses().courseWork().list(**query_params).execute()
+            course_work_list = request.get("courseWork", [])
+            next_page_token = request.get("nextPageToken", None)
+            return course_work_list, next_page_token
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+
+
 
 
 if __name__ == '__main__':
