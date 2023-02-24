836559949988af96dd4e11ac18229ee183eb497e
Liav <liavt242@gmail.com>
2023-02-24 21:31:03 +0200

    student user # added student_submissions funcs

--- src/gcc_student.py
+++ src/gcc_student.py
@@ -1,3 +1,5 @@
+from googleapiclient.errors import HttpError
+
 from gcc_base import GccBase
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -14,3 +16,44 @@ class Student(GccBase):
         if role != 'student':
             raise gcc_exceptions.InvalidRole()
         super().__init__(role, ref_cache_month, work_space, email)
+
+
+    def reclaim_submission(self, course_id: str, course_work_id: str, submission_id: str) -> bool:
+        """
+
+        :param course_id:
+        :param course_work_id:
+        :param submission_id:
+        :return:
+        """
+        try:
+            self.classroom.courses().courseWork().studentSubmissions().reclaim(
+                courseId=course_id,
+                courseWorkId=course_work_id,
+                id=submission_id
+            ).execute()
+            return True
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
+
+
+    def tern_in_submission(self, c, course_id: str, course_work_id: str, submission_id: str) -> bool:
+        """
+
+        :param c:
+        :param course_id:
+        :param course_work_id:
+        :param submission_id:
+        :return:
+        """
+        try:
+            self.classroom.courses().courseWork().studentSubmissions().ternIn(
+                courseId=course_id,
+                courseWorkId=course_work_id,
+                id=submission_id
+            ).execute()
+            return True
+        except HttpError as error:
+            self.logger.error('An error occurred: %s' % error)
+            return False
